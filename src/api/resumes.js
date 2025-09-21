26ca4bb10185ac0511149a816201dc9d6601637e
Carina6ST <karina199606@gmail.com>
2025-09-21 21:10:01 +0100

    Ensure /resumes/upload and token header

--- src/api/resumes.js
+++ src/api/resumes.js
@@ -1,9 +1,7 @@
 import api from "./client";
-
 export async function uploadResume(file) {
   const fd = new FormData();
   fd.append("file", file);
-
   const { data } = await api.post("/resumes/upload", fd, {
     headers: { "Content-Type": "multipart/form-data" },
   });
