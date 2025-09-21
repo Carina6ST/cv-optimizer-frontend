43c665e5b9f6596914a97cfab42cfd88189e850a
Carina6ST <karina199606@gmail.com>
2025-09-21 10:44:34 +0100

    Fix API baseURL to point to backend root

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000",
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "https://cv-optimizer-backend-6qbt.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
